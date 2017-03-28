6e5eb240eb6306fe96344c1d58c9a62de9e14e7a
Ekenmer <ekenmer@gmail.com>
2017-03-28 11:41:14 +0200

    Create placeHolder.c

---
+++ placeHolder.c
@@ -0,0 +1,25 @@
+/*
+Kerrigham & Ritchie:The C Programming Language 2ed.
+001 | Pag 010 | Chapter 1 | hello world | helloWorld.c
+by: ekenmer
+=======================================
+abc.
+*/
+
+#include <stdio.h>
+
+int main ( void )
+{
+  int num;
+  int control = 100;
+
+  printf ( "Enter something:" );
+  scanf ( "%d", &num );
+
+  while ( control > num ) {
+    num = ++num;
+    printf ( "%d\n", num );
+  }
+
+return 0;
+}
