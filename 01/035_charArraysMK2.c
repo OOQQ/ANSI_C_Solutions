7b4394037bdc647ab0de8624877436b07ff96313
OOQQ <ooqq@yahoo.com>
2017-10-03 15:58:34 +0200

    updates

--- 01/035_charArraysMK2.c
+++ 01/035_charArraysMK2.c
@@ -7,23 +7,26 @@
 
 #include <stdio.h>
 
-#define MAXLINE 1000 /* maximum input line size */
+#define MAXLINE 1000			/* maximum input line size */
 
-int getline(char line[], int maxline);
-void copy(char to[], char from[]);
+int max;						/* maximum lenght seen so far */
+char line[MAXLINE];				/* current input line */
+char longest[MAXLINE];			/* longest line saved here */
 
-/* print longest input line */
+int getline(void);
+void copy(void);
+
+/* print longest input line, specialized version */
 int main(void) {
-	int len;					/* current line lenght */
-	int max;					/* maximum lenght seen so far */
-	char line[MAXLINE];			/* current input line */
-	char longest[MAXLINE];		/* longest line saved here */
+	int len;
+	extern int max;
+	extern char longest[];
 
 	max = 0;
-	while ((len = getline(line, MAXLINE)) > 0) {
+	while ((len = getline()) > 0) {
 		if (len > max) {
 			max = len;
-			copy(longest, line);
+			copy();
 		}
 	}
 
@@ -34,30 +37,32 @@ int main(void) {
 return 0;
 }
 
-/*  getline: read a line into s, return lenght */
-int getline(char s[], int lim) {
+/*  getline: specialized version */
+int getline(void) {
 	int i, c;
+	extern char line[];
 
-	for (i = 0; i < lim - 1 && (c = getchar()) ! = EOF && c != '\n'; i++) {
-		s[i] = c;
+	for (i = 0; i < MAXLINE -1 && (c = getchar()) != EOF && c != '\n'; ++i) {
+		line[i] = c;
 	}
 
 	if (c == '\n') {
-		s[i] = c;
+		line[i] = c;
 		++i;
 	}
 
-	s[i] = '\0';
+	line[i] = '\0';
 
 return i;
 }
 
-/* copy 'from' into 'to', assumes 'to' is big enough */
-void copy(char to[], char from[]) {
+/* copy specialized version */
+void copy(void) {
 	int i;
+	extern char line[], longest[];
 
 	i = 0;
-	while ((to[i] = from[i]) != '\0') {
+	while ((longest[i] = line[i]) != '\0') {
 		++i;
 	}
 
